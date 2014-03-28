d0911805cbe8bb7048ee0a4d443fd1b506808b67
Jared Hanson <jaredhanson@gmail.com>
2014-03-28 10:32:39 -0700

    Test case for package exports.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,16 @@
 /* global describe, it, expect */
 
-var pkg = require('..');
+var antenna = require('..');
 
 describe('antenna', function() {
   
-  it('should export object', function() {
-    expect(pkg).to.be.an('object');
+  it('should export function', function() {
+    expect(antenna).to.be.a('function');
+  });
+  
+  it('should export create', function() {
+    expect(antenna.create).to.be.a('function');
+    expect(antenna.create).to.equal(antenna);
   });
   
 });
